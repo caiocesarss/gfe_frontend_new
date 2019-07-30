8bc4ccbe14e9cbdc1741abf51d453a3fe38090e8
Caio Siqueira <caiosiqueira@outlook.com>
2019-07-30 09:24:08 -0300

    heroku setup

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3003/api';
+export const BASE_URL = 'https://finexbackend.herokuapp.com/api';
 /* https://finexbackend.herokuapp.com/api */
 
 export const tableOptions = {
