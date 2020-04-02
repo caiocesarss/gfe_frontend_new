f2d25d56a765f350bc1b51472d64a7464da11b11
Caio Siqueira <caiosiqueira@outlook.com>
2020-04-02 20:27:04 -0300

    pre workana #2

--- src/common/CustomToolbarSelect.js
+++ src/common/CustomToolbarSelect.js
@@ -1,72 +1,51 @@
-import React, { Component } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
+import React, { useState } from 'react';
+import { IconButton, Tooltip } from '@material-ui/core';
 import DeleteIcon from "@material-ui/icons/Delete";
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import FilterIcon from "@material-ui/icons/FilterList";
-import { withStyles } from "@material-ui/core/styles";
-import Link from '@material-ui/core/Link';
-import { Redirect } from 'react-router-dom'
 
-const defaultToolbarSelectStyles = {
-   // iconButton: {
-    //  marginRight: "24px",
-    //  top: "50%",
-    //  display: "inline-block",
-    //  position: "relative",
-    //  transform: "translateY(-50%)"
-    //},
-    //deleteIcon: {
-    //  color: "#000"
-    //}
+import Dialog from '../common/Dialog';
+
+const CustomToolbarSelect = ({ onConfirmPayment }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => {
+    throw Error('not implemented');
   };
 
-  class CustomToolbarSelect extends Component {
-    handleClick = (dados) => {
-       // console.log("click! current selected rows", this.props.selectedRows);
-        //console.log(dados);
-        //this.props.setSome(this.props.selectedRows, dados);
-        const selectedRows = this.props.selectedRows;
-    console.log(selectedRows)
+  const handleDialogAccept = () => {
+    setIsOpen(false);
+    onConfirmPayment();
+  };
 
-    /*selectedRows.data.map(val => {
-      const dataIndex = val.dataIndex;
-      allInvoices =+ list[dataIndex].invoice_id+',';
-    })*/
-   return  <Redirect to='/receivables/registrarpgto/2626' />
-      };
+  return (
+    <div className="custom-toolbar-select">
+      <Tooltip title="icon 2">
+        <IconButton onClick={onClick}>
+          <FilterIcon />
+        </IconButton>
+      </Tooltip>
 
-      render() {
-        const { classes, dataList } = this.props;
-        console.log(dataList);
-        const invoiceId = 2626;
-        return (
-          <div className={"custom-toolbar-select"}>
-            <Tooltip title={"icon 2"}>
-              <IconButton className={classes.iconButton} onClick={dados => this.handleClick(dados)}>
-                <FilterIcon className={classes.deleteIcon} />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title={"Registrar Pagamento"}>
-            
-              <IconButton className={classes.iconButton} onClick={this.handleClick}>
-                <MonetizationOnIcon className={classes.deleteIcon} />
-              </IconButton>
-              
-            </Tooltip>
-            <Tooltip title={"Remover"}>
-              <IconButton className={classes.iconButton} onClick={this.handleClick}>
-                <DeleteIcon className={classes.deleteIcon} />
-              </IconButton>
-            </Tooltip>
-          </div>
-        );
-      }
-    }
-    
-    export default withStyles(defaultToolbarSelectStyles, {
-      name: "CustomToolbarSelect"
-    })(CustomToolbarSelect);
-    
-  
+      <Tooltip title="Registrar Pagamento">
+        <IconButton onClick={() => setIsOpen(true)}>
+          <MonetizationOnIcon />
+        </IconButton>
+      </Tooltip>
+
+      <Tooltip title="Remover">
+        <IconButton onClick={onClick}>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+      <Dialog
+        title="Baixar registros"
+        text="Tem certeza que deseja realizar a baixa dos pagamentos selecionados?"
+        open={isOpen}
+        handleClose={() => setIsOpen(false)}
+        handleDialogAccept={handleDialogAccept}
+      />
+    </div>
+  );
+};
+
+export default CustomToolbarSelect;
